a314da1d2901ae104583b9704d5cc082cad0a0fb
GauravS99 <g.sharma0909@gmail.com>
2019-01-05 02:00:52 -0500

    modified about page

--- app/components/About.js
+++ app/components/About.js
@@ -4,7 +4,7 @@ import {Toolbar, Footer} from './Shared';
 export class About extends React.Component{
   render() {
 	return (
-	  <div>
+	  <div className="verticalFlex fullHeight">
 		<Toolbar/>
 		<AboutContent/>
 		<Footer/>
@@ -36,26 +36,30 @@ class AboutContent extends React.Component{
 						 sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
 						  anim id est laborum.</p>
 					</div>
-					<div>
-						<div className= "displayText regularText">
-						   Meet The Chef
-						</div>
-						<p className="regularText paragraph paragraphText" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-						sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-						 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-						 ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-						 in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-						 sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-						  anim id est laborum.</p>
-					</div>
-					<div>
-						<div className= "displayText regularText">
-						   Contact
-						</div>
-						<p className="regularText paragraph paragraphText">chef@tasteofhome.com</p>
-					</div>
 				</div>
 			</div>   			
 		);
 	}
 }
+
+/**
+	<div>
+		<div className= "displayText regularText">
+		   Meet The Chef
+		</div>
+		<p className="regularText paragraph paragraphText" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+		 ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+		 in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+		 sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+		  anim id est laborum.</p>
+	</div>
+
+	<div>
+						<div className= "displayText regularText">
+						   Contact
+						</div>
+						<p className="regularText paragraph paragraphText">chef@tasteofhome.com</p>
+					</div>
+*/
--- app/components/Menu.js
+++ app/components/Menu.js
@@ -3,12 +3,28 @@ import {Toolbar, Footer} from './Shared';
 
 export class Menu extends React.Component{
   render() {
+  	let source = require("../../images/menu.png");
     return (
-      <div>
+      <div className="verticalFlex fullHeight">
         <Toolbar/>
+        <MenuContent/>
         <Footer/>
       </div>
     );
   }
 	
+}
+
+class MenuContent extends React.Component{
+	render(){
+		let source = require("../../images/menu.png");
+		return (
+			<div className="content">
+				<img
+				src={source}
+				alt="menu"
+				/>
+			</div>
+		);
+	}
 }
